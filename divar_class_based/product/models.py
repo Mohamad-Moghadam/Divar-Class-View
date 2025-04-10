51b94e02a345d78d0e7cde707fecb899e8fc3d22
Mohamad Reza <m.reza.s.moghadam@gmail.com>
2025-04-10 23:18:10 +0330

    🎉 product models added

--- divar_class_based/product/models.py
+++ divar_class_based/product/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from user.models import User
 
-# Create your models here.
+
+class Product(models.Model):
+    name= models.CharField(max_length= 100)
+    price= models.BigIntegerField()
+    description= models.TextField()
+    date_of_publish= models.DateTimeField(auto_now= True)
+    seller= models.ForeignKey(User, on_delete=models.PROTECT, related_name= 'seeler_of_each_product')
