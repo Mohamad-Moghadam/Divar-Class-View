9805ad76d9552272de74a3c423eef6155183b037
Mohamad Reza <m.reza.s.moghadam@gmail.com>
2025-04-10 23:33:01 +0330

    🎉 retrieve update delete url added

--- divar_class_based/product/urls.py
+++ divar_class_based/product/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from product.views import ListCreateView
+from product.views import ListCreateView, RetrieveUpdateDestroyView
 
 
 urlpatterns = [
     path('list-create', ListCreateView.as_view()),
+    path('retreive-update-delete', RetrieveUpdateDestroyView.as_view()),
 ]
