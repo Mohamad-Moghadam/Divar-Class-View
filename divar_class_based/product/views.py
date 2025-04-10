caffdfc33774d00bbf3137abaf1816a5d828c63c
Mohamad Reza <m.reza.s.moghadam@gmail.com>
2025-04-10 23:31:49 +0330

    🎉 retrieve update delete view added

--- divar_class_based/product/views.py
+++ divar_class_based/product/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
-from rest_framework.generics import ListCreateAPIView
+from rest_framework.generics import ListCreateAPIView,RetrieveUpdateDestroyAPIView
 from product.models import Product
 from product.serializers import ProductSerializer
 
 
 class ListCreateView(ListCreateAPIView):
+    queryset= Product.objects.all()
+    serializer_class= ProductSerializer
+
+class RetrieveUpdateDestroyView(RetrieveUpdateDestroyAPIView):
     queryset= Product.objects.all()
     serializer_class= ProductSerializer
