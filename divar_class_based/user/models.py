d9587a9eafe3e6bb0839060e5f4a875895598533
Mohamad Reza <m.reza.s.moghadam@gmail.com>
2025-04-10 21:35:45 +0330

    🎉 user model added

--- divar_class_based/user/models.py
+++ divar_class_based/user/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
 
-# Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=100)
+    number = PhoneNumberField()
+    location= models.TextField()
+    national_id= models.PositiveIntegerField(unique= True)
