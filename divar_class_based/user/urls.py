2d131bbbc9bcefd72094e47961a6dbe5a1acc8f9
Mohamad Reza <m.reza.s.moghadam@gmail.com>
2025-04-10 23:09:29 +0330

    🎉 all user urls added

--- divar_class_based/user/urls.py
+++ divar_class_based/user/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from user.views import ListCreateView
+from user.views import ListCreateView, RetrieveUpdateDestroyView
 
 
 urlpatterns = [
-    path('create-list', ListCreateView.as_view())
+    path('create-list', ListCreateView.as_view()),
+    path('retrieve-update-delete/<int:pk>', RetrieveUpdateDestroyView.as_view()),
 ]
